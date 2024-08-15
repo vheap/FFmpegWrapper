ac407dd2c3d97f4902d72fe34a33030afc97d43c
Mayion <sykole.egy@gmail.com>
2024-08-16 02:29:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using FFmpegWrapper;
+using System.Collections.Concurrent;
+
+
+int MaxConcurrent = 1; // Maximum number of files concurrently scanned.
+SemaphoreSlim SL = new SemaphoreSlim(MaxConcurrent);
+ConcurrentBag<Task> TasksCache = new ConcurrentBag<Task>();
+
+/* A quick and easy take on an FFmpeg wrapper. Supports lightweight threading options and thread-safe concurrency
+ * For larger operations, like batching. Use as is or create your own commands for the process. */
+void UnitTesting()
+{
+    QueueFiles(new string[] { "filePath" });
+    Console.ReadLine();
+}
+
+async void QueueFiles(string[] Files)
+{
+    foreach (string File in Files)
+    {
+        await SL.WaitAsync();
+        TasksCache.Add(ProcessFile(File, SL));
+    }
+    await Task.WhenAll(TasksCache);
+    // All files have been processed.
+}
+
+async Task ProcessFile(string FilePath, SemaphoreSlim SL)
+{
+    try
+    {
+        if (FilePath == null || !File.Exists(FilePath)) return;
+        var ffmpegWrapper = new Wrapper(@"ffprobePath");
+        var cancellationTokenSource = new CancellationTokenSource();
+        var attributes = await ffmpegWrapper.GetVideoStreamXML(FilePath, cancellationTokenSource.Token);
+
+        Console.WriteLine($"Task Result ffprobWrapper for: {Path.GetFileName(FilePath)}\n\n{attributes}");
+    }
+    finally
+    {
+        SL.Release();
+
+    }
+}
+
+UnitTesting();
+
+
+
---
+++ Wrapper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpegWrapper
+{
+    public class Wrapper
+    {
+        private string ffprobePath;
+        private string xmlArguments = "-v quiet -print_format json -show_format -show_streams -print_format xml";
+        public Wrapper(string ffprobePath)
+        {
+            this.ffprobePath = ffprobePath;
+        }
+
+        private async Task<string> RunFFProbeCommandAsync(string arguments, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<string>();
+
+            /* Setting up the parameters for the ffprobe/ffmpeg process. */
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = ffprobePath,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                },
+                EnableRaisingEvents = true
+            };
+
+            /* Making sure to queue the output for the threading. */
+            var output = new ConcurrentQueue<string>();
+            process.OutputDataReceived += (sender, args) => { if (args.Data != null) output.Enqueue(args.Data); };
+            process.ErrorDataReceived += (sender, args) => { if (args.Data != null) output.Enqueue(args.Data); };
+            process.Exited += (sender, args) => tcs.TrySetResult(string.Join(Environment.NewLine, output));
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            using (cancellationToken.Register(() =>
+            {
+                if (!tcs.Task.IsCompleted)
+                {
+                    tcs.TrySetCanceled();
+                    try
+                    {
+                        process.Kill();
+                    }
+                    catch { /* Ignore exceptions if the process is already terminated */ }
+                }
+            }))
+            {
+                try
+                {
+                    return await tcs.Task.ConfigureAwait(false);
+                }
+                finally
+                {
+                    process.WaitForExit();
+                    process.Dispose();
+                }
+            }
+        }
+
+        /* Validate video state. FFmpeg lacks the proper tools for it, so FFProbe is the only valid option. */
+        public async Task<string> IsValidVideo(string videoPath, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var output = await RunFFProbeCommandAsync($"-v error -i \"{videoPath}\"", cancellationToken);
+                return output;
+            }
+            catch { }
+            return string.Empty;
+        }
+
+        /* Extract and return the XML stream for the media file. */
+        public async Task<string> GetVideoStreamXML(string videoPath, CancellationToken cancellationToken)
+        {
+            var output = await RunFFProbeCommandAsync($"\"{videoPath}\" {xmlArguments}", cancellationToken);
+            return output;
+        }
+
+    }
+}
